20decd3bd49acba5b3b45f913522e3ea6dc0ee91
shivambh1717 <shivambhargav1717@gmail.com>
2024-11-19 17:52:31 -0500

    Added test case

--- main_test.go
+++ main_test.go
@@ -24,3 +24,27 @@ func TestCountVowels(t *testing.T) {
 		})
 	}
 }
+
+func TestGCD(t *testing.T) {
+	tests := []struct {
+		name   string
+		a, b   int
+		result int
+	}{
+		{"Both Zero", 0, 0, 0},
+		{"One Zero", 0, 10, 10},
+		{"Equal Numbers", 5, 5, 5},
+		{"Coprime Numbers", 17, 13, 1},
+		{"Common Divisor", 56, 98, 14},
+		{"Negative Numbers", -48, 18, 6},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := GCD(tt.a, tt.b)
+			if got != tt.result {
+				t.Errorf("GCD(%d, %d) = %d; want %d", tt.a, tt.b, got, tt.result)
+			}
+		})
+	}
+}
